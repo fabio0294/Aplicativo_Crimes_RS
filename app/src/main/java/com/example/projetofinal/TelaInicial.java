a11130572c4ae771f2bba6725d6285a2d78b1985
fabio <fabiosantos0294@hotmail.com>
2021-05-17 11:12:46 -0300

    .

--- app/src/main/java/com/example/projetofinal/TelaInicial.java
+++ app/src/main/java/com/example/projetofinal/TelaInicial.java
@@ -7,12 +7,25 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 public class TelaInicial extends Fragment{
     private View root;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         root = inflater.inflate(R.layout.fragment_inicio, container, false);
+
+        SwipeRefreshLayout str = root.findViewById(R.id.swipeTelaInicial);
+        str.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                str.setRefreshing(true);
+
+                str.setRefreshing(false);
+
+            }
+        });
+
         return root;
     }
 }
