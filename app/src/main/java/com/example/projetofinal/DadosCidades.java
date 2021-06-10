dde93a69d569bd1e6329e5bfa9d2c82411778f80
fabio <fabiosantos0294@hotmail.com>
2021-06-10 11:18:01 -0300

    .

--- app/src/main/java/com/example/projetofinal/DadosCidades.java
+++ app/src/main/java/com/example/projetofinal/DadosCidades.java
@@ -8,6 +8,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -41,7 +43,7 @@ public class DadosCidades extends Fragment implements InterfaceDados.DataView {
             }
         });
 
-        Button bt = root.findViewById(R.id.buttonOpenMap);
+        ImageView bt = root.findViewById(R.id.buttonOpenMap);
         bt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
